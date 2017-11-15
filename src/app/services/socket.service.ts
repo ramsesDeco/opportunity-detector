280075b738582f12113464c2fdfd2b14ab0e105a
ramsesDeco <ramses.deco@gmail.com>
2017-11-14 18:23:57 -0600

    socket for production

--- src/app/services/socket.service.ts
+++ src/app/services/socket.service.ts
@@ -4,13 +4,20 @@ import { Http, Headers, RequestOptionsArgs, RequestOptions } from '@angular/http
 
 import { Observable } from 'rxjs/Observable';
 import * as io from 'socket.io-client';
+import { environment } from '../../environments/environment';
+
 
 @Injectable()
 export class SocketService {
-  private url = 'http://localhost:3000';
+  private url: string;
   private socket: any;
 
   constructor(private http: Http, options: RequestOptions) {
+    if (environment.production) {
+      this.url = '';
+    } else {
+      this.url = 'http://localhost:3000';
+    }
     this.socket = io(this.url);
   }
 
